fbda16ab67c5d2412a0d09c471c9cd2b6a4ec528
karthik <karthik>
2017-06-08 16:44:02 +0530

    eee

---
+++ src/components/firstcomponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    Component
+} from 'react';
+
+export default class FirstComponent
+extends Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return ( <
+            div > < div > My First React Component <
+            /div></div >
+        )
+    }
+}
+
+//const SecondComponent = (props) => {
+//    return ( <
+//        div > < div > My Second React Component <
+//        /div></div >
+//    )
+//}
+//
+//export SecondComponent;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import FirstComponent from './components/firstcomponent';
+import React, {
+    Component
+} from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render( < FirstComponent / > ,
+    document.querySelector('.container'));
